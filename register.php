05148bb99e27f0dbbd6919c391e45cf6a6a1a110
Danthology <397053880@qq.com>
2018-04-25 21:37:42 +0800

    注册验证 from Danthology

---
+++ register.php
@@ -0,0 +1,47 @@
+<?php
+include 'configure.php';
+include 'session.php';
+session_set_save_handler('_session_open','_session_close','_session_read','_session_write','_session_destroy','_session_gc');
+session_start();
+if($_POST[check]==$_SESSION[check])
+{
+	$hand=mysqli_connect("$db_host","$db_user","$db_pwd")or die('数据库连接失败');
+	mysqli_select_db($hand,"$db_name")or die('数据库无此库');
+	$sql="select user from password where user='$_POST[user]'";
+	$result=mysqli_query($hand,$sql);
+	$row=mysqli_num_rows($result);
+	if($row==0)
+	{
+    $sql_nickname="select nickname from account_user where nickname='$_POST[nickname]'";
+		$result_nickname=mysqli_query($hand,$sql_nickname);
+		$row_nickname=mysqli_num_rows($result_nickname);
+		if($row_nickname==0)
+		{
+			$pwd=md5($_POST[pwd]);
+			$sql="insert into password values('$_POST[user]','$_POST[nickname]','$pwd')";
+			$result=mysqli_query($hand,$sql);
+			echo "<script language=\"JavaScript\">
+			window.alert('注册成功');
+			window.location.href='login.html';</script>";
+		}
+		else
+		{
+			echo "<script language=\"JavaScript\">
+			window.alert('此昵称已被使用');
+			window.location.href='register.html';</script>";
+		}
+	}
+	else
+	{
+		echo "<script language=\"JavaScript\">
+		window.alert('此用户名已注册');
+		window.location.href='register.html';</script>";
+	}
+}
+else
+{
+	echo "<script language=\"JavaScript\">
+	window.alert('验证码错误');
+	window.location.href='register.html';</script>";
+}
+?>
