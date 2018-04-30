120a480eecd902f49694023d0522ea44fbd09df4
Azurak <byron0511@outlook.com>
2018-04-30 14:31:32 +0800

    修正逻辑不完善

--- newteam.php
+++ newteam.php
@@ -21,6 +21,7 @@ if(isset($_POST['cid'])&&isset($_POST['name'])&&isset($_POST['num'])){
     $result=mysqli_query($hand,$sql);
     $row=mysqli_num_rows($result);
     if($row==0) {
+
         $query2="SELECT uid FROM account_user where user='$user'";
         $r2=mysqli_query($hand,$query2);
         $a2=mysqli_fetch_array($r2,MYSQLI_ASSOC);
@@ -28,6 +29,11 @@ if(isset($_POST['cid'])&&isset($_POST['name'])&&isset($_POST['num'])){
         $tcid=$a2["uid"];
         $time=time();
         $sql="insert into contest_team (`cid`, `created`,`name`, `intro`,`peoplenum`,`tcid`,`status`) values('$cid','$time','$team','$intro','$num','$tcid',1)";
+        $query1="SELECT tid FROM contest_team where name='$user'";
+        $r1=mysqli_query($hand,$query1);
+        $a1=mysqli_fetch_array($r1,MYSQLI_ASSOC);
+        $tid=$a1['tid'];
+        $sql2="insert into contest_join (`cid`, `created`,`tid`, `uid`,`user`,`status`) values('$cid','$time','$tid','$tcid','$user',1)";
         $result=mysqli_query($hand,$sql);
         $return=["msgCode"=>"1"];
         echo json_encode($return);
