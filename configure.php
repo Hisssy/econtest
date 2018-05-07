3f6d503dc0b91e7491c381d60261b65b18ced8e7
YOhonour <33690471+YOhonour@users.noreply.github.com>
2018-05-07 20:16:29 +0800

    Add files via upload

---
+++ configure.php
@@ -0,0 +1,11 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * User: Lenovo
+ * Date: 2018/5/2
+ * Time: 23:52
+ */
+$db_user="root";
+$db_pwd="Xv8*VAB!";
+$db_host="120.78.154.228 ";
+$db_name="contest";
