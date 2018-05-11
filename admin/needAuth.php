28b51e47ccb9808f71d731fd8a4faa35facfc0db
Azurak <byron0511@outlook.com>
2018-05-11 15:21:41 +0800

    修复登录问题

--- admin/needAuth.php
+++ admin/needAuth.php
@@ -2,19 +2,18 @@
 /**
  * Created by PhpStorm.
  * User: Byron
- * Date: 2018/4/30
- * Time: 9:50
+ * Date: 2018/5/11
+ * Time: 15:20
  */
-
 
 //需要登录的页面都用此头文件
 
-include 'session.php';
-include 'configure.php';
+include '../session.php';
+include '../configure.php';
 session_set_save_handler($handler, true);
 session_start();
 if (!$_SESSION["user"]) {
     echo "<script language=\"JavaScript\">
   window.alert('请登陆');
-  window.location.href='login.php';</script>";
+  window.location.href='login.html';</script>";
 }
